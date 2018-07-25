ef4def63dba9faf69a3350e8fb481ba2337bebd0
fabrice zaoui <fabrice.zaoui@edf.fr>
2018-07-25 21:03:26 +0200

    character encoding problem for m2

--- fig_Apropos.R
+++ fig_Apropos.R
@@ -1,15 +1,15 @@
 #########################################
 ### Fonction de visualisation des donnees de peches utilisees
-#### 
+####
 #########################################
 
 figAP_hist_p0 <- function(binwidth=2)
 {
   D <- read.table("data/DATA TRF_AllY.csv", sep=";", dec=',', header=TRUE)
   p <- plot_ly(D, x = ~N100m2_0, type = "histogram", nbinsx=ceiling(max(D$N100m2_0, na.rm=TRUE)/binwidth), hoverinfo="x+y") %>%
-        layout(xaxis = list(title = 'Densites de 0+ (N/100m²)'),
+        layout(xaxis = list(title = 'Densites de 0+ (N/100m2)'),
            yaxis = list(title = 'Nombre'))
-  
+
 	return(p)
 	}
 
@@ -18,9 +18,9 @@ figAP_hist_p1 <- function(binwidth=2)
 {
   D <- read.table("data/DATA TRF_AllY.csv", sep=";", dec=',', header=TRUE)
   p <- plot_ly(D, x = ~N100m2_1, type = "histogram", nbinsx=ceiling(max(D$N100m2_1, na.rm=TRUE)/binwidth), hoverinfo="x+y") %>%
-    layout(xaxis = list(title = 'Densites de 1+ (N/100m²)'),
+    layout(xaxis = list(title = 'Densites de 1+ (N/100m2)'),
            yaxis = list(title = 'Nombre'))
-  
+
   return(p)
 }
 
@@ -30,13 +30,8 @@ figAP_hist_pAd <- function(binwidth=2)
 {
   D <- read.table("data/DATA TRF_AllY.csv", sep=";", dec=',', header=TRUE)
   p <- plot_ly(D, x = ~N100m2_Ad, type = "histogram", nbinsx=ceiling(max(D$N100m2_Ad, na.rm=TRUE)/binwidth), hoverinfo="x+y") %>%
-    layout(xaxis = list(title = 'Densites de Ad (N/100m²)'),
+    layout(xaxis = list(title = 'Densites de Ad (N/100m2)'),
            yaxis = list(title = 'Nombre'))
-  
+
   return(p)
 }
-
-
-
-
-
