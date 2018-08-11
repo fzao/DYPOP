efb01aed7347706d673ebc7f8218698529191f09
fabrice zaoui <fabrice.zaoui@edf.fr>
2018-08-11 17:17:03 +0200

    change color & size for subtitle

--- ui.R
+++ ui.R
@@ -9,7 +9,7 @@ shinyUI(
             fluidRow(
                      column(4, align="left",
                             HTML('<h1 style="color: #FFA02F; background-color: #FFFFFF;">DYPOP</h1>'),
-                            HTML('<h5 style="color: #FFA02F; background-color: #FFFFFF;">Partage des résultats d\'un modèle de dynamique de population de truite</h5>')),
+                            HTML('<h4 style="color: #A5C226; background-color: #FFFFFF;">Partage des résultats d\'un modèle de dynamique de population de truite</h5>')),
                      column(8, align="right",
                             img(height=100, width=65, src="EDF_RetD.png"),
                             img(height=100, width=97, src="Irstea.png"))
@@ -42,9 +42,9 @@ shinyUI(
                                  fluidRow(
                                           column(2, align = "left",
                                                  br(),br(),
-                                                  wellPanel(h4(strong("PARAMETERS")),sliderInput("slider1", label = h3("T 10"), min = 9., max = 17., value = 10, step = 0.5),
-                                                  sliderInput("slider2", label = h3("T 90"), min = 1., max = 7., value = 5., step = 0.5),
-                                                  sliderInput("slider3", label = h3("% Cache"), min = 0., max = 7., value = 3., step = 0.5)
+                                                  wellPanel(h4(strong("PARAMETERS")),sliderInput("slider1", label = h3("T 10"), min = 9., max = 17., value = 10, step = 1.),
+                                                  sliderInput("slider2", label = h3("T 90"), min = 1., max = 7., value = 5., step = 1.),
+                                                  sliderInput("slider3", label = h3("% Cache"), min = 0., max = 7., value = 3., step = 1.)
                                                   )),
                                           column(10, align = "left",
                                                  br(),br(),br(),
