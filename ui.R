db035f9c1be65a459ab7342be1d1c4a126a5bcc1
fabrice zaoui <fabrice.zaoui@edf.fr>
2018-09-21 10:17:07 +0200

    step size is 0.5 now

--- ui.R
+++ ui.R
@@ -43,9 +43,9 @@ shinyUI(
                                           column(2, align = "left",
                                                  br(),br(),
                                                   wellPanel(h4(strong("PARAMETRES")),
-                                                            sliderInput("slider1", label = h3("T 10"), min = 9., max = 17., value = 10, step = 1.),
-                                                            sliderInput("slider2", label = h3("T 90"), min = 1., max = 7., value = 5., step = 1.),
-                                                            sliderInput("slider3", label = h3("% Cache"), min = 0., max = 7., value = 3., step = 1.)
+                                                            sliderInput("slider1", label = h3("T 10"), min = 9., max = 17., value = 10, step = 0.5),
+                                                            sliderInput("slider2", label = h3("T 90"), min = 1., max = 7., value = 5., step = 0.5),
+                                                            sliderInput("slider3", label = h3("% Cache"), min = 0., max = 7., value = 3., step = 0.5)
                                                   )),
                                           column(10, align = "left",
                                                  br(),br(),br(),
